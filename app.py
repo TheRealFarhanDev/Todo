c73da67610013dd043e8010712f4af6236b88337
Mohammed Farhan <mohammedfarhan8612@gamil.com>
2024-12-27 20:13:18 +0530

    minor Changes, debug mode

--- app.py
+++ app.py
@@ -83,5 +83,5 @@ def done(sno):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=False)
  
